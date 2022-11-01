c1fa022deee25ede5cd6632e8626a05445c9ea4f
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-11-01 21:47:32 +0530

    messaging channels users and scheduling messages works

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from slack_sdk import WebClient
 from typing import List
 from dotenv import load_dotenv
 import os
+import re
+from datetime import datetime
 
 load_dotenv(".env")
 
@@ -23,41 +25,49 @@ def get_command_data(cmd_data: List) -> dict:
     return data_dict
 
 
+def get_channels_and_users(text: str) -> List[str]:
+    channels_users = re.findall(r"<([#@])(.*?)\|(.*?)>", text)
+    return channels_users
+
+
+def get_message(text: str) -> str:
+    re_obj = re.compile(r"<[#@].*?\|.*>")
+    message = re_obj.sub("", text)
+    actual_message = " ".join(message.split("+"))
+    return actual_message
+
+
 @app.post("/message")
-async def message_members(request: Request, response: Response, data=Body(...)):
+async def message_members_channels(
+    request: Request, response: Response, data=Body(...)
+):
     raw_body = await request.body()
     body = raw_body.decode("utf-8")
     command_data = [unquote(text) for text in body.split("&")]
-    data_dict = get_command_data(command_data)
-    try:
-        members = data_dict["text"].split("+")
-    except ValueError:
-        members = data_dict["text"]
-    team_members = client.users_list()
-    member_id_dict = {
-        member["name"]: member["id"] for member in team_members["members"]
-    }
 
-    for member in members:
-        if member[1:] in member_id_dict:
-            client.chat_postMessage(channel=member_id_dict[member[1:]], text="Hello")
+    data_dict = get_command_data(command_data)
+    channels_users = get_channels_and_users(data_dict["text"])
+    message = get_message(data_dict["text"])
+    for entity in channels_users:
+        client.chat_postMessage(channel=entity[1], text=message, as_user=True)
 
     return {"Status": "Success"}
 
 
-@app.post("/message-channels")
-async def message_channel(request: Request, response: Response, data=Body(...)):
+@app.post("/message_schedule")
+async def schedule_message(request: Request, response: Response, data=Body(...)):
     raw_body = await request.body()
     body = raw_body.decode("utf-8")
     command_data = [unquote(text) for text in body.split("&")]
+
     data_dict = get_command_data(command_data)
-    print(data_dict)
-    try:
-        channels = data_dict["text"].split("+")
-    except ValueError:
-        channels = data_dict["text"]
+    channels = get_channels_and_users(data_dict["text"])
+    message = get_message(data_dict["text"])
+    print(message)
+    date_time = re.findall(r"\d{1,2}/\d{1,2}/\d{2} \d{1,2}:\d{0,2}:\d{0,2}", message)[0]
+    time_epoch = datetime.strptime(date_time, r"%m/%d/%y %H:%M:%S").timestamp()
+    print(channels)
 
-    for channel_name in channels:
-        client.chat_postMessage(channel=channel_name[1:], text="Hello")
-
+    for entity in channels:
+        client.chat_scheduleMessage(channel=entity[1], text=message, post_at=time_epoch)
     return {"Status": "Success"}
