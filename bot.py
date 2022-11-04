52d52bc066df7e5c4a8c049552082a02a79d7659
Deepak Dinesh <d.deepakdinesh13@gmail.com>
2022-11-04 16:05:18 +0530

    shortcuts not working | message schedule and messaging works

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from fastapi import Body, FastAPI, Request, Response
+from fastapi import Body, FastAPI, Request, Response, status
 from urllib.parse import unquote
 from slack_sdk import WebClient
 from typing import List
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import os
 import re
 from datetime import datetime
+import pytz
+import json
 
 load_dotenv(".env")
 
@@ -44,13 +46,17 @@ async def message_members_channels(
     raw_body = await request.body()
     body = raw_body.decode("utf-8")
     command_data = [unquote(text) for text in body.split("&")]
-
+    print(command_data)
     data_dict = get_command_data(command_data)
     channels_users = get_channels_and_users(data_dict["text"])
     message = get_message(data_dict["text"])
     for entity in channels_users:
-        client.chat_postMessage(channel=entity[1], text=message, as_user=True)
-
+        if entity[0] == "#":
+            channels_members = client.conversations_members(channel=entity[1])
+            for member in channels_members["members"]:
+                client.chat_postMessage(channel=member, text=message, as_user=True)
+        else:
+            client.chat_postMessage(channel=entity[1], text=message, as_user=True)
     return {"Status": "Success"}
 
 
@@ -61,13 +67,96 @@ async def schedule_message(request: Request, response: Response, data=Body(...))
     command_data = [unquote(text) for text in body.split("&")]
 
     data_dict = get_command_data(command_data)
-    channels = get_channels_and_users(data_dict["text"])
+    channels_users = get_channels_and_users(data_dict["text"])
     message = get_message(data_dict["text"])
     print(message)
     date_time = re.findall(r"\d{1,2}/\d{1,2}/\d{2} \d{1,2}:\d{0,2}:\d{0,2}", message)[0]
     time_epoch = datetime.strptime(date_time, r"%m/%d/%y %H:%M:%S").timestamp()
-    print(channels)
 
-    for entity in channels:
-        client.chat_scheduleMessage(channel=entity[1], text=message, post_at=time_epoch)
+    for entity in channels_users:
+        if entity[0] == "#":
+            channels_members = client.conversations_members(channel=entity[1])
+            for member in channels_members["members"]:
+                client.chat_scheduleMessage(
+                    channel=member, text=message, post_at=time_epoch
+                )
+        else:
+            client.chat_scheduleMessage(
+                channel=entity[1], text=message, post_at=time_epoch
+            )
     return {"Status": "Success"}
+
+
+@app.post("/message_schdedule_tz")
+async def schedule_message_in_user_timzone(
+    request: Request, response: Response, data=Body(...)
+):
+    raw_body = await request.body()
+    body = raw_body.decode("utf-8")
+    command_data = [unquote(text) for text in body.split("&")]
+
+    data_dict = get_command_data(command_data)
+    channels_users = get_channels_and_users(data_dict["text"])
+    message = get_message(data_dict["text"])
+    date_time = datetime.strptime(
+        re.findall(r"\d{1,2}/\d{1,2}/\d{2} \d{1,2}:\d{0,2}:\d{0,2}", message)[0]
+    )
+
+    for entity in channels_users:
+        if entity[0] == "#":
+            channels_members = client.conversations_members(channel=entity[1])
+            for member in channels_members["members"]:
+                user_tz = client.users_info(member)["tz"]
+                convtd_date_time = date_time.astimezone(pytz.timezone(user_tz))
+                time_epoch = convtd_date_time.timestamp()
+                client.chat_scheduleMessage(
+                    channel=member, text=message, post_at=time_epoch
+                )
+        else:
+            user_tz = client.users_info(entity[1])["tz"]
+            convtd_date_time = date_time.astimezone(pytz.timezone(user_tz))
+            time_epoch = convtd_date_time.timestamp()
+            client.chat_scheduleMessage(
+                channel=entity[1], text=message, post_at=time_epoch
+            )
+    return {"Status": "Success"}
+
+
+@app.post("/shortcut-test")
+async def schedule_message_in_user_timzone(
+    request: Request, response: Response, data=Body(...)
+):
+    raw_body = await request.body()
+    raw_form = await request.form()
+    body = unquote(raw_body.decode("utf-8"))
+    payload = json.loads(raw_form.get("payload"))
+    resp = client.views_open(
+        trigger_id=payload["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "modal-id",
+            "title": {"type": "plain_text", "text": "Awesome Modal"},
+            "submit": {"type": "plain_text", "text": "Submit"},
+            "blocks": [
+                {
+                    "type": "input",
+                    "block_id": "b-id",
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Input label",
+                    },
+                    "element": {
+                        "action_id": "a-id",
+                        "type": "plain_text_input",
+                    },
+                }
+            ],
+        },
+    )
+    if (
+        payload["type"] == "view_submission"
+        and payload["view"]["callback_id"] == "modal-id"
+    ):
+        # Handle a data submission request from the modal
+        submitted_data = payload["view"]["state"]["values"]
+    return status.HTTP_200_OK
